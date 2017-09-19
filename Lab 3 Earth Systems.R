af35a53857f9b478f200cfc599644de406249208
Beatrice Rodewald <beatrice.rodewald@vanderbilt.edu>
2017-09-19 13:27:47 -0500

    Some work has been done

--- Lab 3 Earth Systems.R
+++ Lab 3 Earth Systems.R
@@ -1,14 +1,26 @@
+---
+  title: 'EES 2510 Earth Systems Lab #3'
+author: "Beatrice Rodewald"
+date: 'Lab: Wed. Sept. 20'
+output:
+  pdf_document: default
+html_document: default
+word_document: default
+subtitle: 'Ice Core Data'
+---
+
+  
 library('readxl')
 db= read_excel('Vostok_Data.xlsx', col_names= TRUE)
 head(db)
 db = db [2:nrow(db), ]
 attach (db)
-plot(depth, ice_age, main= "Vostok Ice Core", xlab= "Depth pf Core (m)", ylab= "age (ka)", col= "red", pch = 19, cex = .25)
+plot(depth, ice_age, main= "Vostok Ice Core", xlab= "Depth of Core (m)", ylab= "age (ka)", col= "red", pch = 19, cex = .25)
 points(depth, gas_age, col= "blue", cex = .25)
-legend("topleft", col= c("red", "blue"), c("ice", "gas"), pch=19, bty="n")
+legend("topleft", col= c("red", "blue"), c("ice, CO2", "gas, CH4"), pch=19, bty="n")
 par(mar= c(5, 5, 2, 5))
 par(new=TRUE)
 with (db, plot(ice_age, CO2, type="l", col="blue", ylab= NA, yaxt= "n", xlab = NA))
 axis(side=4)
 mtext(side=4, line=3, "Methane (ppb)")
-legend ("topleft", col=c("red", "blue"), c("CO2", "CH4"), pch= 19, bty="n")
+
